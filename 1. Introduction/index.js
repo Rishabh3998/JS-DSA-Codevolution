83610b709125c0bae33572a4fc4a59e6d5ecf688
Rishabh Kumar <rsk1998200@gmail.com>
2024-12-21 20:11:51 +0530

    Intorduction of course

---
+++ 1. Introduction/index.js
@@ -0,0 +1,54 @@
+// Codevolution: https://youtube.com/playlist?list=PLC3y8-rFHvwjPxNAKvZpdnsr41E0fCMMP&si=QEsAd6Gn_n0IEQKr
+
+// This repo will contain DSA concepts, implemented using Javascript.
+
+// What is a Data structure ?
+
+// - A Data structure is a way to store and organize data so that it can be used efficiently.
+
+// - It is a collection of data values, the relationships among them, and the functions or operations that
+//   can be applied to the data.
+
+// Why learn Data structures ?
+
+// - Almost every application we build involves data that is modelled in a certain way.
+
+// - To efficiently manage the data we need Data structures.
+
+// - Imagine walking into the library only to find out there is no efficient organization of books and they
+//   are stored randomly.
+
+// - The difference between a function taking a few milliseconds vs a few seconds or even minutes comes
+//   down to the selection of right Data structure.
+
+// - Data structure help you solve problems in a more efficient way, both in terms of time and memory.
+
+// - Learning about Data structures also help you gain a more profound understanding of things you are
+//   already aware of.
+
+// For example:
+
+// - DOM = uses Tree Data structure
+
+// - Browser back and forward button = uses Stack Data structure.
+
+// - OS job scheduling = uses Queue Data structure.
+
+// Course Overview:
+
+// Learn the built-in Data structure
+
+// 1. Arrays
+// 2. Objects
+// 3. Sets
+// 4. Maps
+
+// Custom Data structures:
+
+// 1. Stacks
+// 2. Queues
+// 3. Circular Queues
+// 4. Linked Lists
+// 5. Hash tables
+// 6. Trees
+// 7. Graphs
